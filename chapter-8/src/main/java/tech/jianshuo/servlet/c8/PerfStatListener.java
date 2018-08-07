133483f3f6369b101b3d9822868bf477f8d4d28a
zhen.yu <iszhenyu@yeah.net>
2018-08-08 00:03:44 +0800

    ..

--- chapter-8/src/main/java/tech/jianshuo/servlet/c8/PerfStatListener.java
+++ chapter-8/src/main/java/tech/jianshuo/servlet/c8/PerfStatListener.java
@@ -7,6 +7,8 @@ import javax.servlet.annotation.WebListener;
 /**
  * @author zhen.yu
  * Created on 2018-08-07
+ *
+ * 两个方法之间的执行时间即为一个url请求的耗时
  */
 @WebListener
 public class PerfStatListener implements ServletRequestListener {
